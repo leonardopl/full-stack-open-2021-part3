531bda8ff2337ab4195681bcf209b57c26d7c349
Leonardo <leonardopl@users.noreply.github.com>
2021-06-30 14:01:27 +0200

    adjust cors use

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
-app.use(cors())
-
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
