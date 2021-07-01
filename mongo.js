0e187e33eb04d2ba423f0bd9379edb57587e1346
Leonardo <leonardopl@users.noreply.github.com>
2021-07-01 20:21:53 +0200

    submit exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3 || process.argv.length === 4 || process.argv.length > 5) {
+  console.log('This program takes as arguments the password for the database and the name and number of the person to be added to the phonebook, in this order. To only display the current list of people in the phonebook, only the password is needed. To add a new person, please provide all arguments.')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.qofip.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
